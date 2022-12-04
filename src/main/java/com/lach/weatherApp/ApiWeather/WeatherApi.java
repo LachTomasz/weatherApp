932ff99fa8ba39a078de662cef959376ecc492d3
LachTomasz <lach.tomasz@gmail.com>
2022-12-04 11:08:50 +0100

    Creates a client to download weather data of given city.

--- src/main/java/com/lach/weatherApp/ApiWeather/WeatherApi.java
+++ src/main/java/com/lach/weatherApp/ApiWeather/WeatherApi.java
@@ -1,7 +1,6 @@
 package com.lach.weatherApp.ApiWeather;
 
 import com.lach.weatherApp.City;
-import com.lach.weatherApp.Weather;
 import com.lach.weatherApp.WeatherbitObject.WeatherbitResponse;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
@@ -9,24 +8,25 @@ import jakarta.json.bind.JsonbBuilder;
 
 public class WeatherApi {
 
-    private static final String API_URL_TEMPLATE = "http://api.weatherbit.io/v2.0/forecast/daily?";
+    private static final String API_URL_TEMPLATE = "http://api.weatherbit.io/v2.0/forecast/daily?&city=";
     private static final String API_KEY = "3a592d44641446cd87518392b3497ebb";
 
     private final ConnectionFactory connectionFactory;
     private final Jsonb jsonb;
 
-    public WeatherApi(){
+    public WeatherApi() {
         this(new ConnectionFactory(), JsonbBuilder.create());
     }
+
     public WeatherApi(ConnectionFactory connectionFactory, Jsonb jsonb) {
         this.connectionFactory = connectionFactory;
         this.jsonb = jsonb;
     }
 
-    public WeatherbitResponse weatherbitResponse(City city){
-        String requestUrl = API_URL_TEMPLATE + city.name() + API_KEY;
+    public WeatherbitResponse weatherbitResponse(City city) {
+        String requestUrl = API_URL_TEMPLATE + city.name() + "&key=" + API_KEY;
         WeatherbitResponse weatherbitResponse;
-        try (HttpConnection connection = connectionFactory.build(requestUrl)){
+        try (HttpConnection connection = connectionFactory.build(requestUrl)) {
             String response = connection.response();
             weatherbitResponse = jsonb.fromJson(response, WeatherbitResponse.class);
         }
