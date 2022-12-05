888e262ada0b8514c7fbd8f5f9a86620b1e1ff58
LachTomasz <lach.tomasz@gmail.com>
2022-12-05 23:17:23 +0100

    Created a new class BestWeatherController.

---
+++ src/main/java/com/lach/weatherApp/BestWeatherController.java
@@ -0,0 +1,10 @@
+package com.lach.weatherApp;
+
+public class BestWeatherController {
+
+    private final String cityName;
+
+    public BestWeatherController(String cityName) {
+        this.cityName = cityName;
+    }
+}
