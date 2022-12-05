0018b4b907b5881addd759231ebcba1ca658c81d
LachTomasz <lach.tomasz@gmail.com>
2022-12-05 22:05:01 +0100

    Creates repository and methods.

--- src/main/java/com/lach/weatherApp/Weather.java
+++ src/main/java/com/lach/weatherApp/Weather.java
@@ -2,12 +2,18 @@ package com.lach.weatherApp;
 
 public class Weather {
 
+    private final String city;
     private final Float averageTemp;
     private final Float windSpeed;
 
-    public Weather(Float averageTemp, Float windSpeed) {
+    public Weather(String city, Float averageTemp, Float windSpeed) {
+        this.city = city;
         this.averageTemp = averageTemp;
         this.windSpeed = windSpeed;
+    }
+
+    public String getCity() {
+        return city;
     }
 
     public Float getAverageTemp() {
