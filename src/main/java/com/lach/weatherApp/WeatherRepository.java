171d91056be8c35af86297b4652a33c765b15afd
LachTomasz <lach.tomasz@gmail.com>
2022-12-05 22:05:01 +0100

    Creates repository and methods.

--- src/main/java/com/lach/weatherApp/WeatherRepository.java
+++ src/main/java/com/lach/weatherApp/WeatherRepository.java
@@ -8,9 +8,23 @@ import java.util.Map;
 @Repository
 public class WeatherRepository {
 
-    private final Map<Long, WeatherbitResponse> idToWeather;
+    private final Map<City, WeatherbitResponse> idToWeather;
 
-    public WeatherRepository(Map<Long, WeatherbitResponse> idToWeather) {
-        this.idToWeather = idToWeather;
+    public WeatherRepository(Map<City, WeatherbitResponse> idToWeather) { this.idToWeather = idToWeather; }
+
+    public WeatherbitResponse save(WeatherbitResponse weatherbitResponse){
+        idToWeather.put(City.valueOf(weatherbitResponse.getCityName()),weatherbitResponse);
+        return idToWeather.get(City.valueOf(weatherbitResponse.getCityName()));
     }
+
+    public WeatherbitResponse update(WeatherbitResponse weatherbitResponse){
+        idToWeather.put(City.valueOf(weatherbitResponse.getCityName()), weatherbitResponse);
+        return idToWeather.get(City.valueOf(weatherbitResponse.getCityName()));
+    }
+
+    public WeatherbitResponse find(City city){ return idToWeather.get(city);}
+
+    public void delete(City city){idToWeather.remove(city);}
+
+
 }
